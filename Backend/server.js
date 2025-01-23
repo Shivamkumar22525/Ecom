3dd61ef2b67a991703dc2c61e260c8582343d569
Shivamkumar22525 <shivam22525@gmail.com>
2025-01-23 13:41:03 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,29 @@
-console.log("shivam");
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+
+//App config
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDB();
+connectCloudinary();
+
+//Middlewares
+
+app.use(express.json());
+app.use(cors());
+
+
+//Api endpoints
+
+app.get('/',(req,res)=>{
+    res.send("working");
+})
+
+app.listen(port,()=>{
+    console.log(`server is working on port: ${port}`);
+});
